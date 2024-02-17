fd4098a7be807012ef40bb601d13d620ecb23a4e
SolaRich <ivan100500707@gmail.com>
2024-02-17 20:39:36 +0300

    broke test

--- src/test/java/ru/netology/postmanecho/PostmanEchoTest.java
+++ src/test/java/ru/netology/postmanecho/PostmanEchoTest.java
@@ -18,6 +18,6 @@ class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", org.hamcrest.Matchers.equalTo(text));
+                .body("value", org.hamcrest.Matchers.equalTo(text));
     }
 }
